a054166b125ebf55a5026635cf0b3b0e135f17e4
Naufaldi <m.naufaldi.aldi@gmail.com>
2022-05-28 18:37:40 +0700

    camera update

--- camera.py
+++ camera.py
@@ -7,9 +7,7 @@ class Camera(pygame.sprite.Group):
         super().__init__()
         self.display = pygame.display.get_surface()
         self.offset = pygame.math.Vector2()
-        
-        self.rect = pygame.Rect(CAMERA_MARGIN_LEFT, CAMERA_MARGIN_TOP, WINDOW_WIDTH - (CAMERA_MARGIN_LEFT * 2), WINDOW_HEIGHT - (CAMERA_MARGIN_TOP * 2))
-   
+           
     def custom_draw(self, player):
         self.offset.x = player.rect.centerx - (WINDOW_WIDTH / 2)
         self.offset.y = player.rect.centery - (WINDOW_HEIGHT / 2)
