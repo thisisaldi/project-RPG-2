f6de05c1f1abd0814751c70c200a30b876832f1c
Irsyad <azcat01@yahoo.co.id>
2022-06-03 17:54:02 +0700

    change map

--- terrain.py
+++ terrain.py
@@ -4,8 +4,8 @@ from config import *
 class Terrain(pygame.sprite.Sprite):
     def __init__(self, group):
         super().__init__(group)
-        self.image = pygame.image.load('assets/map.jpg').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (2381, 1195))
+        self.image = pygame.image.load('assets/map3.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * MAP_SCALE, self.image.get_height() * MAP_SCALE))
         
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
