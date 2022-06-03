70b7817848f8d89d71c69b2047a8f2f91a080896
Irsyad <azcat01@yahoo.co.id>
2022-06-03 21:40:47 +0700

    add masked orc

--- enemy.py
+++ enemy.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-import random
+import json
 from config import *
 from creature import Creature
 
@@ -10,6 +10,18 @@ class Enemy(Creature):
         self.player = player
         self.enemies = enemies
         self.distance = pygame.math.Vector2()
+        with open('enemy_stats.json', 'r') as enemy_stats:
+            self.stats = json.load(enemy_stats)
+            
+        self.attacking = False
+
+    def config_stats(self, name):
+        self.name = name
+        self.hp = self.stats[self.name]['HP']
+        self.damage = self.stats[self.name]['Damage']
+        self.level = self.stats[self.name]['Level']
+        self.range = self.stats[self.name]['Range']
+        self.type = self.stats[self.name]['Type']
         
     def collision(self, dir):
         for enemy in self.enemies:
@@ -25,14 +37,17 @@ class Enemy(Creature):
                     elif self.direction.y < 0:
                         self.rect.top = enemy.rect.bottom
                 self.idle = True
+
+    def check_distance(self, range):
+        self.distance = math.sqrt((self.direction.x)**2 + (self.direction.y)**2)
+        return (self.distance < range and self.distance >= 60)
                         
     def move(self):
 
         self.direction.x = self.player.rect.x - self.rect.x
         self.direction.y = self.player.rect.y - self.rect.y
-        self.distance = math.sqrt((self.direction.x)**2 + (self.direction.y)**2)
-
-        if self.distance >= 60 and self.distance < 400:
+        
+        if self.check_distance(self.range) and not self.attacking:
             if self.direction.magnitude() != 0:
                 self.direction.normalize()
                 self.direction.scale_to_length(self.speed)
@@ -43,13 +58,14 @@ class Enemy(Creature):
             self.rect.y += self.direction.y
             self.collision('y')
 
-        elif self.distance < 60:
+        elif self.distance < 60 and not self.attacking:
             self.attacking = True
+            self.damaged = False
             self.idle = False
             self.direction.x = 0
             self.direction.y = 0
 
-        else:
+        elif not self.attacking:
             self.idle = True
             self.attacking = False
             self.direction.x = 0
@@ -63,7 +79,8 @@ class Enemy(Creature):
 class Goblin(Enemy):
     def __init__(self, group, player, enemies, pos):
         super().__init__(group, player, enemies)
-
+        self.config_stats('Goblin')
+        self.display = pygame.display.get_surface()
         self.image_idle_right = []
         self.image_idle_left = []
         self.image_run_right = []
@@ -135,20 +152,132 @@ class Goblin(Enemy):
                 self.anim_delay = 0
         
         elif self.attacking:
+            if self.index >= 5:
+                if self.rect.colliderect(self.player.rect) and not self.damaged:
+                    self.player.hp -= self.damage
+                    self.damaged = True
             if self.index >= len(self.image_attacking_right):
                 self.index = 0
+                self.attacking = False
             if self.right:
                 self.image = self.image_attacking_right[self.index]
             else:
                 self.image = self.image_attacking_left[self.index]
             self.anim_delay += 1
+            if self.anim_delay >= ENEMY_ATTACKING_DELAY:
+                self.index += 1
+                self.anim_delay = 0
+
+
+
+    def update(self):
+        if self.hp <= 0:
+            self.alive = False
+
+        if self.alive:
+            self.animation()
+            self.move()
+            # pygame.draw.rect(self.display, 'white', self.rect, 2)
+
+class MaskedOrc(Enemy):
+    def __init__(self, group, player, enemies, pos):
+        super().__init__(group, player, enemies)
+        self.config_stats('Masked Orc')
+        self.display = pygame.display.get_surface()
+        self.image_idle_right = []
+        self.image_idle_left = []
+        self.image_run_right = []
+        self.image_run_left = []
+        self.image_attacking_right = []
+        self.image_attacking_left = []
+
+        for i in range(0, 4):
+            self.image = pygame.image.load(f'assets/enemies/masked_orc_idle_anim_f{i}.png').convert_alpha() # png belom diganti
+            self.image = pygame.transform.scale(self.image, ENEMY_SIZE)
+
+            self.image_idle_right.append(self.image)
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.image_idle_left.append(self.image)
+        
+        self.rect = self.image.get_rect()
+
+        for i in range(0, 4):
+            self.image = pygame.image.load(f'assets/enemies/masked_orc_run_anim_f{i}.png').convert_alpha() # png belom diganti
+            self.image = pygame.transform.scale(self.image, ENEMY_SIZE)
+
+            self.image_run_right.append(self.image)
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.image_run_left.append(self.image)
+        
+        self.rect = self.image.get_rect()
+
+        for i in range(0, 4):
+            self.image = pygame.image.load(f'assets/enemies/masked_orc_idle_anim_f{i}.png').convert_alpha()
+            self.image = pygame.transform.scale(self.image, ENEMY_SIZE)
+
+            self.image_attacking_right.append(self.image)
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.image_attacking_left.append(self.image)
+        
+        # self.rect = self.image.get_rect()
+
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.index = 0
+        self.anim_delay = 0
+        self.speed = ENEMY_SPEED
+        self.idle = False
+        self.right = True
+    
+    def animation(self):
+        if not self.idle and not self.attacking:
+            if self.index >= len(self.image_run_right):
+                self.index = 0
+            if self.right:
+                self.image = self.image_run_right[self.index]
+            else:
+                self.image = self.image_run_left[self.index]
+            self.anim_delay += 1
             if self.anim_delay >= ENEMY_DELAY:
                 self.index += 1
                 self.anim_delay = 0
-    
-    def attack(self):
-        pass
+
+        elif self.idle:
+            if self.index >= len(self.image_idle_right):
+                self.index = 0
+            if self.right:
+                self.image = self.image_idle_right[self.index]
+            else:
+                self.image = self.image_idle_left[self.index]
+            self.anim_delay += 1
+            if self.anim_delay >= ENEMY_DELAY:
+                self.index += 1
+                self.anim_delay = 0
+        
+        elif self.attacking:
+            if self.index >= 5:
+                if self.rect.colliderect(self.player.rect) and not self.damaged:
+                    self.player.hp -= self.damage
+                    self.damaged = True
+            if self.index >= len(self.image_attacking_right):
+                self.index = 0
+                self.attacking = False
+            if self.right:
+                self.image = self.image_attacking_right[self.index]
+            else:
+                self.image = self.image_attacking_left[self.index]
+            self.anim_delay += 1
+            if self.anim_delay >= ENEMY_ATTACKING_DELAY:
+                self.index += 1
+                self.anim_delay = 0
+
+
 
     def update(self):
-        self.move()
-        self.animation()
+        if self.hp <= 0:
+            self.alive = False
+        print(self.alive)
+        if self.alive:
+            self.animation()
+            self.move()
+            # pygame.draw.rect(self.display, 'white', self.rect, 2)
