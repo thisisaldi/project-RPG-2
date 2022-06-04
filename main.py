6c1603e754b7379b2984c330255abd9418da099b
Naufaldi <m.naufaldi.aldi@gmail.com>
2022-06-04 22:13:37 +0700

    add to-do list

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from game import Game
 
+"""
+    TO-DO:
+    
+    - Combat Development:
+        - Player and Enemy HP
+        - Player attack and skill system
+        - Other fixes
+    - Sound FX
+    - Map Optimization (Obstacle and others)
+
+
+"""
+
+
+
+
 if __name__ == '__main__':
     game = Game()
     game.run()
