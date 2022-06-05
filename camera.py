194bb2acf0942dc79a4ae8a98fe0d7d1e54f12cf
Naufaldi <m.naufaldi.aldi@gmail.com>
2022-06-05 14:36:45 +0700

    gradient

--- camera.py
+++ camera.py
@@ -8,11 +8,12 @@ class Camera(pygame.sprite.Group):
         self.display = pygame.display.get_surface()
         self.offset = pygame.math.Vector2()
         self.enemies = enemies
+        self.gradient = pygame.image.load('assets/gradient.png').convert_alpha()
+        self.gradient = pygame.transform.scale(self.gradient, WINDOW_SIZE)
            
     def custom_draw(self, player):
         self.offset.x = player.rect.centerx - (WINDOW_WIDTH / 2)
         self.offset.y = player.rect.centery - (WINDOW_HEIGHT / 2)
-        
         for sprite in self.sprites():
             if sprite.alive:
                 offset_pos = sprite.rect.topleft - self.offset
@@ -24,4 +25,5 @@ class Camera(pygame.sprite.Group):
                 pygame.draw.rect(self.display, 'red', sprite.hp1, 5)
                 pygame.draw.rect(self.display, 'white', sprite.hp2, 5)
                 
+        self.display.blit(self.gradient, (0, 0))
                 
