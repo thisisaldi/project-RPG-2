75727ad9825d69fb4903626ae155dab6e3545412
Irsyad <azcat01@yahoo.co.id>
2022-06-09 21:45:21 +0700

    update TODO

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from game import Game
 
 """
-    TO-DO:
-    
+    TO-DO (MAIN):
+    - Survival/Wave (3 Waves)
+    - Enemy Spawn (Wave 1 : 5 Goblin, Wave 2 : 5 Masked Orc, Wave 3 : Boss)
+    - 1 Stage Kotak
+    - Pake pedang doang
+    - Health Drop
+    - Level Up
+    - Game Over
+    - GUI HP, Level, Stats
+    - Convert Exe
+
+    TO-DO (Side):
+    - Story
     - Combat Development:
         - Player and Enemy HP
         - Player attack and skill system
         - Other fixes
-    - Sound FX
+    - Enemy SFX
     - Map Optimization (Obstacle and others)
     - Lighting
     - Enemy animations
-
+    - Main Menu
 """
 
 if __name__ == '__main__':
