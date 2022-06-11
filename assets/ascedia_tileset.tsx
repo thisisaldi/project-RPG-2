44c9223d9048ebe9e89809527b87ef35ccfe7e3e
Daffa Nurrizqi <ndaffaraihan@gmail.com>
2022-06-11 12:21:48 +0700

    added big door and minor arena map changes

--- assets/ascedia_tileset.tsx
+++ assets/ascedia_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="ascedia_tileset" tilewidth="16" tileheight="16" tilecount="324" columns="18">
- <image source="../../ascedia_tileset.png" width="288" height="288"/>
+<tileset version="1.8" tiledversion="1.8.5" name="ascedia_tileset" tilewidth="16" tileheight="16" tilecount="504" columns="18">
+ <image source="../../ascedia_tileset.png" width="288" height="448"/>
 </tileset>
