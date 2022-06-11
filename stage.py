393260004409c129b5172fd69b5a7d12715d1dbe
Irsyad <azcat01@yahoo.co.id>
2022-06-11 20:54:36 +0700

    spawn boss

--- stage.py
+++ stage.py
@@ -18,8 +18,9 @@ class Stage:
         self.terrain = Terrain([self.visible])
         self.player = Player([self.visible], self.enemies)
         sfx.ambience_sound()
-        # self.enemy1 = Goblin([self.visible, self.enemies], self.player, self.enemies, (random.randint(-100, 100), random.randint(-100, 100)))
+        self.enemy1 = Goblin([self.visible, self.enemies], self.player, self.enemies, (random.randint(-100, 100), random.randint(-100, 100)))
         self.enemy2 = MaskedOrc([self.visible, self.enemies], self.player, self.enemies, (random.randint(-50, 50), random.randint(-50, 50)))
+        self.enemy3 = Boss([self.visible, self.enemies], self.player, self.enemies, (random.randint(-150, 150), random.randint(-150, 150)))
         
 
     def run(self, interval, now):
@@ -30,7 +31,7 @@ class Stage:
         self.visible.update()
 
         if interval - now >= 5000:
-            # Goblin([self.visible, self.enemies], self.player, self.enemies, (random.randint(-200, 200), random.randint(-200, 200)))
+            Goblin([self.visible, self.enemies], self.player, self.enemies, (random.randint(-200, 200), random.randint(-200, 200)))
             MaskedOrc([self.visible, self.enemies], self.player, self.enemies, (random.randint(-200, 200), random.randint(-200, 200)))
 
             return interval
